a28400105d035b66982e53d43178dce0524c8430
Ily5 <53183641+Ily5@users.noreply.github.com>
2020-08-04 13:35:24 +0300

    Update application.py

--- fixture/application.py
+++ fixture/application.py
@@ -24,7 +24,7 @@ class Application:
         }
 
         self.wd = webdriver.Remote(
-            command_executor='http://10.129.0.112:4444/wd/hub',
+            command_executor='http://10.129.0.112:4444/',
             desired_capabilities=capabilities
         )
 
