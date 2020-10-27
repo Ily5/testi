23a3a3a72edcc87d600fe6dee1d2ee1d0ea6b088
ikoshkin <ikoshkin@neuro.net>
2020-10-27 15:56:40 +0300

    test commit

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 from fixture.application import Application
 from fixture.database import Connector, MongoConnector
+from test_3.fixture.application_3 import ApplicationNewVersion
 import pytest
 import json
 import sys
@@ -36,20 +37,37 @@ def app(request):
 
 
 @pytest.fixture(scope="session")
+def app_3(request):
+    global fixture
+    if fixture is None:
+        browser = request.config.getoption("--browser")
+        with open(request.config.getoption("--config")) as cfg:
+            config = json.load(cfg)
+            fixture = ApplicationNewVersion(browser=browser, cms_url=config["CmsUrl"], api_url=config["ApiEndpoint"],
+                                  api_headers=config["ApiHeaders"], api_methods=config["ApiMethods"],
+                                  pool_api=config["PoolApiUrl"], p_api_headers=config["PoolApiHeaders"],
+                                  project=config["ProjectId"], rwdb=config["Postgres"]["RW"],
+                                  cms_db=config["Postgres"]["CMS"], mdb=config["Mongo_client"])
+            fixture.session.login(username=config["UsernameCms"], password=config["PasswordCms"])
+
+    def done():
+        try:
+            fixture.session.logout(username=config["UsernameCms"])
+            fixture.cancel()
+        except:
+            pass
+
+    request.addfinalizer(done)
+    return fixture
+
+
+@pytest.fixture(scope="session")
 def db(request):
     with open(request.config.getoption("--config")) as cfg:
         config = json.load(cfg)
         fixture = Connector(data=config["Postgres"]["RW"])
     request.addfinalizer(fixture.cancel)
     return fixture
-
-# @pytest.fixture(scope="session")
-# def api(request):
-#     with open(request.config.getoption("--config")) as cfg:
-#         config = json.load(cfg)
-#         fixture = Connector(data=config["Postgres"]["RW"])
-#     request.addfinalizer(fixture.cancel)
-#     return fixture
 
 
 @pytest.fixture(scope="session")
@@ -64,4 +82,4 @@ def mdb(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="Remote")
-    parser.addoption("--config", action="store", default=ROOT_DIR + "\config_prod.json")
+    parser.addoption("--config", action="store", default=ROOT_DIR + "\config_test.json")
