b9151a210dc2ef508e4ef4ef2a7686e067c79e60
renat <renat.devetyarov@bk.ru>
2020-12-04 17:32:59 +0300

    Доработана фикстура изменения настроек агента на дефолтные Добавлена фикстура генерации рандомной строки

--- test_3/conftest.py
+++ test_3/conftest.py
@@ -1,5 +1,7 @@
 import pytest
 import requests
+import string
+import random
 import json
 from test_3.fixture.api import APIClientV3
 
@@ -30,6 +32,20 @@ def params_agent_uuid(api_v3):
 @pytest.fixture
 def default_settings_agent(api_v3, params_agent_uuid):
     yield
-    data = {"flag": f"{api_v3.test_data['agent_flag']}"}
+    test_data = api_v3.test_data
+    data = {"flag": f"{test_data['agent_flag']}",
+            "name": f"{test_data['agent_name']}",
+            "recall_count": f"{test_data['recall_count']}",
+            "routing_channel_limit": None,
+            "asr": f"{test_data['asr']}",
+            "tts": f"{test_data['tts']}",
+            "language": f"{test_data['language']}"}
+
     path = api_v3.path_end_point['put_change_agent_settings']
     api_v3.request_send(method="PUT", path=path, json=data, params=params_agent_uuid)
+
+
+@pytest.fixture
+def random_str_generator(size=random.randint(3, 256),
+                         chars=string.ascii_uppercase + string.digits + string.ascii_lowercase):
+    return ''.join(random.choice(chars) for _ in range(size))
