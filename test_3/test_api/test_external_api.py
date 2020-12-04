080e874cb259118dd2ad896c42b1c3519faffef7
renat <renat.devetyarov@bk.ru>
2020-12-04 17:34:06 +0300

    Минорные изменения

--- test_3/test_api/test_external_api.py
+++ test_3/test_api/test_external_api.py
@@ -3,7 +3,6 @@ import allure
 import requests
 from random import randint
 
-
 @pytest.mark.parametrize('login', ['rdevetiarov@neuro.net', 'sddsf@neuro.net'])
 @pytest.mark.parametrize('password', [randint(21341231, 312423234234)])
 @allure.feature('Получение токена невалидный данные')
@@ -26,8 +25,6 @@ def test_get_refresh_token(api_v3):
     assert 'refresh_token' in response.json()
 
 
-# todo включить текст как пофиксят
-@pytest.mark.skip(reason='500 ошибка от сервера, https://neuronet.atlassian.net/browse/NP-1325')
 @pytest.mark.parametrize('no_valid_token', [str(randint(12123, 45345343543)), 'sdfgagsdfgaa', '   '])
 @allure.feature('Обновление токена, невалидный токен')
 def test_get_refresh_token_again(api_v3, no_valid_token):
@@ -73,9 +70,22 @@ def test_get_agent_settings_valid(api_v3, params_agent_uuid):
 
 
 @allure.feature('Изменение настроек проекта, валидные данные, agent_uuid')
-def test_change_agent_settings_valid(api_v3, params_agent_uuid, default_settings_agent):
+def test_change_agent_settings_valid(api_v3, params_agent_uuid, default_settings_agent, random_str_generator):
     path = api_v3.path_end_point['put_change_agent_settings']
-    data = {"flag": "new_flag"}
+    data = {"recall_count": 22,
+            "flag": random_str_generator,
+            "routing_channel_limit": 55,
+            "asr": "google",
+            "tts": "oksana@yandex",
+            "language": "en-US"}
     response = api_v3.request_send(method='PUT', path=path, params=params_agent_uuid, json=data)
     assert response.status_code == 200
-    assert response.json()['flag'] == 'new_flag'
+    # assert response.json()['name'] == data['name']
+    assert response.json()['recall_count'] == data['recall_count']
+    assert response.json()['flag'] == data['flag']
+    assert response.json()['routing_channel_limit'] == data['routing_channel_limit']
+    assert response.json()['asr'] == data['asr']
+    assert response.json()['tts'] == data['tts']
+    assert response.json()['language'] == data['language']
+    # assert response.json()['delay'] == data['delay'] #todo узнать про этот параметр
+
