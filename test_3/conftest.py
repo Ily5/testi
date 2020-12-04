56b39bdad77619816b5859b0aa77ebe988f3fe76
renat <renat.devetyarov@bk.ru>
2020-12-04 14:26:46 +0300

    Добавлены новые фикстуры

--- test_3/conftest.py
+++ test_3/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import requests
 import json
 from test_3.fixture.api import APIClientV3
 
@@ -10,11 +11,25 @@ def api_v3(request):
         with open(request.config.getoption("--config")) as cfg:
             config = json.load(cfg)
             fixture = APIClientV3(base_url=config['v3']['api']['api_base_url'],
-                                  company_uuid=config['v3']['test_data']['company_uuid'])
-
-            fixture.token = fixture.get_token(password=config['v3']['auth']['pass'],
-                                              login=config['v3']['auth']['login'])
+                                  test_data=config['v3']['test_data'],
+                                  path_end_point=config['v3']['api']['methods_end_point'])
+            token, refresh_token = fixture.get_api_token(password=config['v3']['auth']['pass'],
+                                                         login=config['v3']['auth']['login'])
+        fixture.token, fixture.refresh_token = token, refresh_token
 
     return fixture
 
-# TODO добавить конфиги v3 в соотвествующие данные
+
+@pytest.fixture
+def params_agent_uuid(api_v3):
+    agent_uuid = api_v3.test_data['agent_uuid']
+    params = {"agent_uuid": f"{agent_uuid}"}
+    return params
+
+
+@pytest.fixture
+def default_settings_agent(api_v3, params_agent_uuid):
+    yield
+    data = {"flag": f"{api_v3.test_data['agent_flag']}"}
+    path = api_v3.path_end_point['put_change_agent_settings']
+    api_v3.request_send(method="PUT", path=path, json=data, params=params_agent_uuid)
