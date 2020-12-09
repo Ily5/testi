9b900df34bf9d60409b1904b4066d0ef438141d1
renat <renat.devetyarov@bk.ru>
2020-12-09 17:23:39 +0300

    Новые тестовые фикстуры, незначительные изменения

--- test_3/conftest.py
+++ test_3/conftest.py
@@ -4,6 +4,7 @@ import string
 import random
 import json
 from test_3.fixture.api import APIClientV3
+from random import randint
 
 
 @pytest.fixture(scope='session')
@@ -45,7 +46,52 @@ def default_settings_agent(api_v3, params_agent_uuid):
     api_v3.request_send(method="PUT", path=path, json=data, params=params_agent_uuid)
 
 
+@pytest.fixture()
+def create_new_rule_agent(request, api_v3, params_agent_uuid):
+    path = api_v3.path_end_point['agent_call_rules']
+    data = {"day": 1, "not_before": "09:00", "not_after": "10:30"}
+    response = api_v3.request_send(method='POST', path=path, json=data, params=params_agent_uuid)
+    time_slot_uuid = response.json()['time_slot_uuid']
+
+    def fin():
+        api_v3.request_send(method='DELETE', path=path, json={"time_slot_uuid": response.json()['time_slot_uuid']})
+
+    request.addfinalizer(fin)
+    return time_slot_uuid
+
+
+@pytest.fixture()
+def create_new_initial_entity_agent(request, api_v3, random_str_generator, params_agent_uuid):
+    path = api_v3.path_end_point['agent_initial_entities']
+    data = {"name": random_str_generator,
+            "synthesis": True}
+    response = api_v3.request_send(method='POST', path=path, params=params_agent_uuid, json=data)
+    initial_entity_uuid = response.json()['initial_entity_uuid']
+
+    def fin():
+        api_v3.request_send(method='DELETE', path=path,
+                            json={'initial_entity_uuid': response.json()['initial_entity_uuid']})
+
+    request.addfinalizer(fin)
+    return initial_entity_uuid
+
+
+@pytest.fixture()
+def create_new_output_entity_agent(request, api_v3, random_str_generator, params_agent_uuid):
+    path = api_v3.path_end_point['agent_output_entities']
+    data = {"name": random_str_generator, "number_cell": randint(2, 9), "entity_type": 'string'}
+    response = api_v3.request_send(method='POST', path=path, params=params_agent_uuid, json=data)
+    output_entity_uuid = response.json()['output_entity_uuid']
+
+    def fin():
+        api_v3.request_send(method='DELETE', path=path,
+                            json={'output_entity_uuid': output_entity_uuid})
+
+    request.addfinalizer(fin)
+    return output_entity_uuid
+
+
 @pytest.fixture
-def random_str_generator(size=random.randint(3, 256),
-                         chars=string.ascii_uppercase + string.digits + string.ascii_lowercase):
+def random_str_generator(size=random.randint(3, 129),
+                         chars=string.ascii_uppercase + string.digits + string.ascii_lowercase + '\t'):
     return ''.join(random.choice(chars) for _ in range(size))
