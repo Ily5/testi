7e20c1456d99e600e5d19259ef0325453e7c522b
renat <renat.devetyarov@bk.ru>
2020-12-14 17:50:31 +0300

    Добавлены новые тесты

--- test_3/test_api/test_pool_api.py
+++ test_3/test_api/test_pool_api.py
@@ -7,6 +7,7 @@ from random import randint
 def test_get_calls_queue(pool_api_v3):
     path = pool_api_v3.path_end_point['get_calls']
     response = pool_api_v3.request_send(path=path)
+    print(response)
     assert response.status_code == 200
 
 
@@ -38,11 +39,56 @@ def test_get_list_queue_calls(pool_api_v3, params_agent_id):
 
 @allure.feature('Отложить все звонки в очереди')
 def test_defer_all_calls(pool_api_v3, params_agent_id):
-    path = pool_api_v3.path_end_point['defer_all_calls']
+    path = pool_api_v3.path_end_point['defer_calls']
     response = pool_api_v3.request_send(method='POST', path=path, params=params_agent_id, json={})
     assert response.status_code == 200
 
 
-@allure.feature('Отложить один звонок, валидный call_id')
-def test_defer_call(pool_api_v3, params_agent_id):
-    pass
+@allure.feature('Вернуить все звонки из отложенных')
+def test_return_all_calls(pool_api_v3, params_agent_id):
+    path = pool_api_v3.path_end_point['return_calls']
+    response = pool_api_v3.request_send(method='POST', path=path, params=params_agent_id, json={})
+    assert response.status_code == 200
+
+
+@allure.feature('Отменить все звонки в очереди')
+def test_cancel_all_calls(pool_api_v3, params_agent_id):
+    path = pool_api_v3.path_end_point['get_list_queue_calls']
+    response = pool_api_v3.request_send(method='DELETE', path=path, params=params_agent_id, json={})
+    assert response.status_code == 200
+
+
+@allure.feature('Сбросить очередь')
+def test_drop_queue_calls(pool_api_v3, params_agent_id):
+    path = pool_api_v3.path_end_point['drop_queue_calls']
+    response = pool_api_v3.request_send(method='POST', path=path, params=params_agent_id, json={})
+    assert response.json() == 200
+
+
+@allure.feature('Получить объект очереди звонков')
+def test_get_full_queue_calls(pool_api_v3, params_agent_id):
+    path = pool_api_v3.path_end_point['get_full_queue']
+    response = pool_api_v3.request_send(path=path, params=params_agent_id)
+    assert response.status_code == 200
+
+
+@allure.feature('Получить все валидные звонки')
+def test_get_all_valid_calls(pool_api_v3, params_agent_id):
+    path = pool_api_v3.path_end_point['get_all_valid_calls']
+    params = {**{"page": "1",
+                 "by_count": "3"}, **params_agent_id}
+    response = pool_api_v3.request_send(path=path, params=params)
+    assert response.status_code == 200
+
+
+@allure.feature('Получение объекта очереди диалогов агента')
+def test_all_dialog_queue(pool_api_v3, params_agent_id):
+    params_agent_id = {'agent_id': '97'}
+    params = {**{"page": "1",
+                 "by_count": "3"}, **params_agent_id}
+    path = pool_api_v3.path_end_point['get_all_dialog_queue']
+    response = pool_api_v3.request_send(path=path, params=params)
+    # todo добавить фикстуру, которая в первом тестовом методе устанавливает лимит каналов =0, массово загружает
+    #  диалоги, в последнем методе вовращает настройки в дефолт
+    print(response)
+    print(response.text)
