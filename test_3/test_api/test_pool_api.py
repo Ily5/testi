676d5a2a575d2b4aa2a26254f8f571f122d29dde
renat <renat.devetyarov@bk.ru>
2020-12-15 15:47:58 +0300

    New tests for poll (queue) api

--- test_3/test_api/test_pool_api.py
+++ test_3/test_api/test_pool_api.py
@@ -1,4 +1,3 @@
-import pytest
 import allure
 from random import randint
 
@@ -7,7 +6,6 @@ from random import randint
 def test_get_calls_queue(pool_api_v3):
     path = pool_api_v3.path_end_point['get_calls']
     response = pool_api_v3.request_send(path=path)
-    print(response)
     assert response.status_code == 200
 
 
@@ -62,7 +60,7 @@ def test_cancel_all_calls(pool_api_v3, params_agent_id):
 def test_drop_queue_calls(pool_api_v3, params_agent_id):
     path = pool_api_v3.path_end_point['drop_queue_calls']
     response = pool_api_v3.request_send(method='POST', path=path, params=params_agent_id, json={})
-    assert response.json() == 200
+    assert response.status_code == 200
 
 
 @allure.feature('Получить объект очереди звонков')
@@ -81,14 +79,47 @@ def test_get_all_valid_calls(pool_api_v3, params_agent_id):
     assert response.status_code == 200
 
 
-@allure.feature('Получение объекта очереди диалогов агента')
-def test_all_dialog_queue(pool_api_v3, params_agent_id):
+@allure.feature('Получение объекта очереди диалогов агента, валидный agent_id')
+def test_all_dialog_queue_valid(pool_api_v3, params_agent_id):
     params_agent_id = {'agent_id': '97'}
     params = {**{"page": "1",
                  "by_count": "3"}, **params_agent_id}
     path = pool_api_v3.path_end_point['get_all_dialog_queue']
     response = pool_api_v3.request_send(path=path, params=params)
-    # todo добавить фикстуру, которая в первом тестовом методе устанавливает лимит каналов =0, массово загружает
-    #  диалоги, в последнем методе вовращает настройки в дефолт
-    print(response)
-    print(response.text)
+    assert response.status_code == 200
+
+
+@allure.feature('Получение размера очереди диалогов агента, валидный agent_id')
+def test_get_dialog_queue_size_valid(pool_api_v3, params_agent_id):
+    params_agent_id = {'agent_id': '97'}
+    path = pool_api_v3.path_end_point['get_dialog_queue_size']
+    response = pool_api_v3.request_send(path=path, params=params_agent_id)
+    assert response.status_code == 200
+
+
+@allure.feature('Отложить все диалоги для проекта')
+def test_defer_all_dialogs(pool_api_v3, params_agent_id):
+    path = pool_api_v3.path_end_point['defer_dialog']
+    response = pool_api_v3.request_send(method='POST', path=path, params=params_agent_id)
+    assert response.status_code == 200
+
+
+@allure.feature('Вернуть все диалоги из отложенных, валидный agent_id')
+def test_return_all_dialog_valid(pool_api_v3, params_agent_id):
+    path = pool_api_v3.path_end_point['return_dialog']
+    response = pool_api_v3.request_send(method='POST', path=path, params=params_agent_id)
+    assert response.status_code == 200
+
+
+@allure.feature('Отменить все далоги для проекта, валидный agent_id')
+def test_cancel_all_dialog_valid(pool_api_v3, params_agent_id):
+    path = pool_api_v3.path_end_point['cancel_all_dialogs']
+    response = pool_api_v3.request_send(method='DELETE', path=path, params=params_agent_id)
+    assert response.status_code == 200
+
+
+@allure.feature('Получить диалоги')
+def test_get_dialogs(pool_api_v3):
+    path = pool_api_v3.path_end_point['get_dialogs']
+    response = pool_api_v3.request_send(path=path)
+    assert response.status_code == 200
