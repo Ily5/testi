6f18c6d4f2dc1dca12649ea6d9589b06b72f6885
Renat Devetyarov <rdevetiarov@neuro.net>
2020-12-18 10:38:56 +0300

    Added class, new asserts

--- test_3/test_api/test_pool_api.py
+++ test_3/test_api/test_pool_api.py
@@ -79,49 +79,62 @@ def test_get_all_valid_calls(pool_api_v3, params_agent_id):
     assert response.status_code == 200
 
 
-@allure.feature('Получение объекта очереди диалогов агента, валидный agent_id')
-def test_all_dialog_queue_valid(pool_api_v3, params_agent_id, creation_queue_dialog):
-    params = {**{"page": "1",
-                 "by_count": "100"}, **params_agent_id}
-    path = pool_api_v3.path_end_point['get_all_dialog_queue']
-    response = pool_api_v3.request_send(path=path, params=params)
-    print(response.text)
-    assert response.status_code == 200
-    assert len(response.json()['dialogs']) == response.json()['total']
-    assert response.json()['total'] == len(creation_queue_dialog)
-    assert len(response.json()['dialogs']) == len(creation_queue_dialog)
+class TestPoolApiDialog:
 
+    @allure.feature('Получение объекта очереди диалогов агента, валидный agent_id')
+    def test_all_dialog_queue_valid(self, pool_api_v3, params_agent_id, creation_queue_dialog):
+        params = {**{"page": "1",
+                     "by_count": "100"}, **params_agent_id}
+        path = pool_api_v3.path_end_point['get_all_dialog_queue']
+        response = pool_api_v3.request_send(path=path, params=params)
+        assert response.status_code == 200
+        assert len(response.json()['dialogs']) == response.json()['total']
+        assert response.json()['total'] == len(creation_queue_dialog)
+        assert len(response.json()['dialogs']) == len(creation_queue_dialog)
 
-@allure.feature('Получение размера очереди диалогов агента, валидный agent_id')
-def test_get_dialog_queue_size_valid(pool_api_v3, params_agent_id):
-    path = pool_api_v3.path_end_point['get_dialog_queue_size']
-    response = pool_api_v3.request_send(path=path, params=params_agent_id)
-    assert response.status_code == 200
+    @allure.feature('Получение размера очереди диалогов агента, валидный agent_id')
+    def test_get_dialog_queue_size_valid(self, pool_api_v3, params_agent_id):
+        path = pool_api_v3.path_end_point['get_dialog_queue_size']
+        response = pool_api_v3.request_send(path=path, params=params_agent_id)
 
+        assert response.status_code == 200
 
-@allure.feature('Отложить все диалоги для проекта')
-def test_defer_all_dialogs(pool_api_v3, params_agent_id):
-    path = pool_api_v3.path_end_point['defer_dialog']
-    response = pool_api_v3.request_send(method='POST', path=path, params=params_agent_id)
-    assert response.status_code == 200
+    @allure.feature('Отложить все диалоги для проекта')
+    def test_defer_all_dialogs(self, pool_api_v3, params_agent_id, creation_queue_dialog):
+        path = pool_api_v3.path_end_point['defer_dialog']
+        response = pool_api_v3.request_send(method='POST', path=path, params=params_agent_id)
+        assert response.status_code == 200
+        response = pool_api_v3.request_send(path=pool_api_v3.path_end_point['get_all_dialog_queue'],
+                                            params={**{"page": "1",
+                                                       "by_count": "100"}, **params_agent_id})
+        assert response.json()['total'] == 0
+        assert len(response.json()['dialogs']) == 0
 
+    @allure.feature('Вернуть все диалоги из отложенных, валидный agent_id')
+    def test_return_all_dialog_valid(self, pool_api_v3, params_agent_id, creation_queue_dialog):
+        path = pool_api_v3.path_end_point['return_dialog']
+        response = pool_api_v3.request_send(method='POST', path=path, params=params_agent_id)
+        assert response.status_code == 200
+        response = pool_api_v3.request_send(path=pool_api_v3.path_end_point['get_all_dialog_queue'],
+                                            params={**{"page": "1",
+                                                       "by_count": "100"}, **params_agent_id})
+        assert len(response.json()['dialogs']) == response.json()['total']
+        assert response.json()['total'] == len(creation_queue_dialog)
+        assert len(response.json()['dialogs']) == len(creation_queue_dialog)
 
-@allure.feature('Вернуть все диалоги из отложенных, валидный agent_id')
-def test_return_all_dialog_valid(pool_api_v3, params_agent_id):
-    path = pool_api_v3.path_end_point['return_dialog']
-    response = pool_api_v3.request_send(method='POST', path=path, params=params_agent_id)
-    assert response.status_code == 200
+    @allure.feature('Отменить все далоги для проекта, валидный agent_id')
+    def test_cancel_all_dialog_valid(self, pool_api_v3, params_agent_id, creation_queue_dialog):
+        path = pool_api_v3.path_end_point['cancel_all_dialogs']
+        response = pool_api_v3.request_send(method='DELETE', path=path, params=params_agent_id)
+        assert response.status_code == 200
+        response = pool_api_v3.request_send(path=pool_api_v3.path_end_point['get_all_dialog_queue'],
+                                            params={**{"page": "1",
+                                                       "by_count": "100"}, **params_agent_id})
+        assert response.json()['total'] == 0
+        assert len(response.json()['dialogs']) == 0
 
-
-@allure.feature('Отменить все далоги для проекта, валидный agent_id')
-def test_cancel_all_dialog_valid(pool_api_v3, params_agent_id):
-    path = pool_api_v3.path_end_point['cancel_all_dialogs']
-    response = pool_api_v3.request_send(method='DELETE', path=path, params=params_agent_id)
-    assert response.status_code == 200
-
-
-@allure.feature('Получить диалоги')
-def test_get_dialogs(pool_api_v3):
-    path = pool_api_v3.path_end_point['get_dialogs']
-    response = pool_api_v3.request_send(path=path)
-    assert response.status_code == 200
+    @allure.feature('Получить диалоги')
+    def test_get_dialogs(self, pool_api_v3):
+        path = pool_api_v3.path_end_point['get_dialogs']
+        response = pool_api_v3.request_send(path=path)
+        assert response.status_code == 200
