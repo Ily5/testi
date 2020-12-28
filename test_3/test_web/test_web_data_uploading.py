a9972effeb81aebbfee01bc8746fd33d048aca6f
Renat <rdevetiarov@neuro.net>
2020-12-28 14:33:41 +0300

    Отладочные тесты

---
+++ test_3/test_web/test_web_data_uploading.py
@@ -0,0 +1,14 @@
+import time
+
+
+class TestWebDataUploading:
+
+    def test_2(self, agent_settings_page):
+        time.sleep(3)
+        time.sleep(3)
+
+    def test_3(self, agent_settings_page):
+        time.sleep(3)
+        agent_settings_page.AnyAgentPage.open_data_uploading_page()
+        path_valid_file = agent_settings_page.test_data['path_to_uploading_file']['valid_file']
+        agent_settings_page.DataUploadingPage.uploading_file(path_valid_file)
