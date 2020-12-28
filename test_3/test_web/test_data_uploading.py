25a067c78088980012c7c09b23fe0e797036f998
Renat <rdevetiarov@neuro.net>
2020-12-28 17:53:05 +0300

    Отладочный тест

--- test_3/test_web/test_data_uploading.py
+++ test_3/test_web/test_data_uploading.py
@@ -12,3 +12,17 @@ class TestWebDataUploading:
         agent_settings_page.AnyAgentPage.open_data_uploading_page()
         path_no_valid_file = agent_settings_page.test_data['path_to_uploading_file']['no_valid_file']
         agent_settings_page.DataUploadingPage.uploading_file(path_no_valid_file)
+
+    def test_sorting(self, agent_settings_page):
+        agent_settings_page.AnyAgentPage.open_data_uploading_page()
+        time.sleep(15)
+        agent_settings_page.DataUploadingPage.set_filer_status(status='success')
+        time.sleep(7)
+        agent_settings_page.DataUploadingPage.set_filer_status(status='all_statuses')
+        time.sleep(7)
+        agent_settings_page.DataUploadingPage.set_filer_status(status='failed')
+        time.sleep(7)
+        agent_settings_page.DataUploadingPage.set_filer_status(status='warning')
+        time.sleep(7)
+        agent_settings_page.DataUploadingPage.set_filer_status(status='loading')
+        time.sleep(7)
