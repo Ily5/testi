d30ab04f4257a3f74119cc95036f405a1d470be8
Renat <rdevetiarov@neuro.net>
2020-12-29 13:24:08 +0300

    Добавлены шаги для отчета в аллюр

--- test_3/fixture/WebFW/AnyPage.py
+++ test_3/fixture/WebFW/AnyPage.py
@@ -1,3 +1,4 @@
+import allure
 from test_3.fixture.BasePage import BasePage
 
 
@@ -5,10 +6,12 @@ class AnyPage(BasePage):
     __account_button = '//app-user-menu[@class="app-user-menu"]'
     __sing_out_button = '//*[@id="mat-menu-panel-0"]//button'
 
+    @allure.step('Логаут из системы')
     def logout(self):
         self._click_by_account_button()
         self.click_by_xpath(self.__sing_out_button)
 
+    @allure.step('Клик по кнопке аккаунт верхний правый угол')
     def _click_by_account_button(self):
         self.click_by_xpath(self.__account_button)
         return self
