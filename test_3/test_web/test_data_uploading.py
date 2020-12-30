27ccd812de66977dddd70e24c6d60be750676d72
Renat <rdevetiarov@neuro.net>
2020-12-30 12:18:09 +0300

    Новые проверки, доработки написанных ранее тестов

--- test_3/test_web/test_data_uploading.py
+++ test_3/test_web/test_data_uploading.py
@@ -16,6 +16,7 @@ class TestWebDataUploading:
         result = agent_settings_page.DataUploadingPage.get_info_n_file(1)
         assert result['status'] == 'SUCCESS'
         assert result['name'] == agent_settings_page.test_data['name_uploading_file']['valid_file']
+        assert result['count_contact'] is not None
         assert data_now in result['time_uploading']
 
     @allure.feature('Загрузка невалидного файла')
@@ -29,6 +30,7 @@ class TestWebDataUploading:
         result = agent_settings_page.DataUploadingPage.get_info_n_file(1)
         assert result['status'] == 'FAILED'
         assert result['name'] == agent_settings_page.test_data['name_uploading_file']['no_valid_file']
+        assert result['error_message'] is not None
         assert data_now in result['time_uploading']
 
     @allure.feature('Изменение сортировки списка загруженных файлов')
@@ -55,8 +57,7 @@ class TestWebDataUploading:
     def test_filters_all_statuses(self, agent_settings_page):
         agent_settings_page.DataUploadingPage.open_data_uploading_page()
 
-        locator = agent_settings_page.DataUploadingPage._DataUploadingPage__all_uploading_file_list + '/div'
-        count_elements = len(agent_settings_page.DataUploadingPage.find_elements(locator))
+        count_elements = agent_settings_page.DataUploadingPage.count_list_files()
         count = 10
         if count > count_elements: count = count_elements
         result = [agent_settings_page.DataUploadingPage.get_info_n_file(i)['status'] for i in range(1, count + 1)]
@@ -69,8 +70,7 @@ class TestWebDataUploading:
         agent_settings_page.DataUploadingPage.open_data_uploading_page()
         agent_settings_page.DataUploadingPage.set_filer_status(status='success')
 
-        locator = agent_settings_page.DataUploadingPage._DataUploadingPage__all_uploading_file_list + '/div'
-        count_elements = len(agent_settings_page.DataUploadingPage.find_elements(locator))
+        count_elements = agent_settings_page.DataUploadingPage.count_list_files()
         count = 10
         if count > count_elements: count = count_elements
         result = [agent_settings_page.DataUploadingPage.get_info_n_file(i)['status'] for i in range(1, count + 1)]
@@ -83,8 +83,7 @@ class TestWebDataUploading:
         agent_settings_page.DataUploadingPage.open_data_uploading_page()
         agent_settings_page.DataUploadingPage.set_filer_status(status='failed')
 
-        locator = agent_settings_page.DataUploadingPage._DataUploadingPage__all_uploading_file_list + '/div'
-        count_elements = len(agent_settings_page.DataUploadingPage.find_elements(locator))
+        count_elements = agent_settings_page.DataUploadingPage.count_list_files()
         count = 10
         if count > count_elements: count = count_elements
         result = [agent_settings_page.DataUploadingPage.get_info_n_file(i)['status'] for i in range(1, count + 1)]
@@ -97,8 +96,7 @@ class TestWebDataUploading:
         agent_settings_page.DataUploadingPage.open_data_uploading_page()
         agent_settings_page.DataUploadingPage.set_filer_status(status='loading')
 
-        locator = agent_settings_page.DataUploadingPage._DataUploadingPage__all_uploading_file_list + '/div'
-        count_elements = len(agent_settings_page.DataUploadingPage.find_elements(locator))
+        count_elements = agent_settings_page.DataUploadingPage.count_list_files()
 
         assert count_elements == 0
 
@@ -107,12 +105,11 @@ class TestWebDataUploading:
         agent_settings_page.DataUploadingPage.open_data_uploading_page()
         agent_settings_page.DataUploadingPage.set_filer_status(status='warning')
 
-        locator = agent_settings_page.DataUploadingPage._DataUploadingPage__all_uploading_file_list + '/div'
-        count_elements = len(agent_settings_page.DataUploadingPage.find_elements(locator))
+        count_elements = agent_settings_page.DataUploadingPage.count_list_files()
 
         assert count_elements == 0
 
-    @allure.feature('Удаление одного успшено загруженного файла из списка')
+    @allure.feature('Удаление одного успшено загруженного файла со статсус success из списка')
     def test_delete_one_valid_file(self, agent_settings_page):
         agent_settings_page.DataUploadingPage.open_data_uploading_page()
         agent_settings_page.DataUploadingPage.set_filer_status(status='success')
@@ -122,8 +119,51 @@ class TestWebDataUploading:
 
         assert before['time_uploading'] != after['time_uploading']
 
+    @allure.feature('Удаление одного успешно загруженного файла со статусом failed из списка')
+    def test_delete_one_no_valid_file(self, agent_settings_page):
+        agent_settings_page.DataUploadingPage.open_data_uploading_page()
+        agent_settings_page.DataUploadingPage.set_filer_status(status='failed')
+
+        no_valid_file_path = agent_settings_page.test_data['path_to_uploading_file']['no_valid_file']
+        agent_settings_page.DataUploadingPage.uploading_file(no_valid_file_path)
+        before = agent_settings_page.DataUploadingPage.get_info_n_file(1)
+
+        agent_settings_page.DataUploadingPage.delete_n_file(1)
+        after = agent_settings_page.DataUploadingPage.get_info_n_file(1)
+
+        assert before['time_uploading'] != after['time_uploading']
+
     @allure.feature('Скачивание образца загрузочного файла')
     def test_download_example(self, agent_settings_page):
         agent_settings_page.DataUploadingPage.open_data_uploading_page()
-        agent_settings_page.DataUploadingPage.download_example()
 
+        locator = agent_settings_page.DataUploadingPage._DataUploadingPage__download_example
+        flag = agent_settings_page.BasePage.check_element_on_page(locator)
+        assert flag
+        # todo добавить скачивание файла и проверку, что он скачался
+        # agent_settings_page.DataUploadingPage.download_example()
+
+    @allure.feature('Скачивание валидного успешно загруженного файла')
+    def test_download_valid_file(self, agent_settings_page):
+        agent_settings_page.AnyAgentPage.open_data_uploading_page()
+        agent_settings_page.DataUploadingPage.set_filer_status(status='success')
+
+        locator = agent_settings_page.DataUploadingPage._DataUploadingPage__all_uploading_file_list + '/div[1]'
+        locator_1 = agent_settings_page.DataUploadingPage._DataUploadingPage__download_valid_file
+        locator += locator_1
+
+        flag = agent_settings_page.BasePage.check_element_on_page(locator)
+        assert flag
+
+        # todo добавить скачивание файла и проверку, что он скачался
+        # agent_settings_page.DataUploadingPage.download_valid_n_file(1)
+
+    @allure.feature('Удаление всех успешно загруженных файлов')
+    def test_delete_all_completed_file(self, agent_settings_page):
+        agent_settings_page.AnyAgentPage.open_data_uploading_page()
+        agent_settings_page.DataUploadingPage.delete_all_completed_uploading()
+        time.sleep(1)
+
+        count_elements = agent_settings_page.DataUploadingPage.count_list_files()
+
+        assert count_elements == 0
