be335e768ba809aac4753a5ac22bfc5e977859d8
Renat <rdevetiarov@neuro.net>
2021-01-13 10:57:24 +0300

    Пробные изменения для отчетов аллюра №6

--- test_3/test/test_smoke/test_release.py
+++ test_3/test/test_smoke/test_release.py
@@ -43,6 +43,18 @@ logs_dict = {}
 #         print('dialog id - ', dialog_id)
 #     print('call uuid - ', db.select_data(table='call', column='dialog_id', sdata='uuid', data=int(dialog_id)))
 #     result = db.execute_call_data(table='dialog_stats', data=dialog_id)
+# synth_phrase_list = [res[1][res[1].find(':') + 4: res[1].find(',') - 1] for res in result if 'nv.synthesize' in res]
+#     for item in [res[1] for res in result if 'nn.log' in res]:
+#         if 'city' in item:
+#             logs_dict['extract_address'] = item
+#         if 'first' in item:
+#             logs_dict['extract_person'] = item
+#         if 'bot' in item:
+#             logs_dict['call_transcription'] = item
+#         if len(item) <= 4:
+#             logs_dict['call_duration'] = item
+#         if 'no_input_timeout' in item:
+#             logs_dict['get_default'] = item
 
 @allure.feature("Smoke 3.0")
 @allure.story("Yandex")
@@ -300,123 +312,211 @@ def test_v3_media_part_yandex_nv_get_default(app_3, db):
 #         dialog_id = db.select_data(table='dialog', column='uuid', sdata='id', data=str(dialog_uuid))[0][0]
 #     result = db.execute_call_data(table='dialog_stats', data=dialog_id)
 #     print(db.select_data(table='call', column='dialog_id', sdata='uuid', data=int(dialog_id)))
+# synth_phrase_list = [res[1][res[1].find(':') + 4: res[1].find(',') - 1] for res in result if 'nv.synthesize' in res]
+#     for item in [res[1] for res in result if 'nn.log' in res]:
+#         if 'city' in item:
+#             logs_dict['extract_address'] = item
+#         if 'first' in item:
+#             logs_dict['extract_person'] = item
+#         if 'bot' in item:
+#             logs_dict['call_transcription'] = item
+#         if len(item) <= 4:
+#             logs_dict['call_duration'] = item
+#         if 'no_input_timeout' in item:
+#             logs_dict['get_default'] = item
 
 
 @allure.feature("Smoke 3.0")
-@allure.story("Проверка медиа части Google")
-def test_v3_media_part_google(app_3, db):
+@allure.story("Проверка медиа части google")
+@allure.title('nv_say')
+def test_v3_media_part_google_say(app_3, db):
     global result
-    with allure.step("nv_say"):
-        for res in result:
-            if 'nv.say' in res:
-                assert any('hello' in d for d in res)
-        if not any('nv.say' in d for d in result):
-            assert False
-    with allure.step("nv_background"):
-        # ____ nv background
-        for res in result:
-            if 'nv.background' in res:
-                assert any('Office_sound' in d for d in res)
-        if not any('nv.background' in d for d in result):
-            assert False
-    # ____ nv play random sound
-    with allure.step("nv.play_random_sound"):
-        for res in result:
-            if 'nv.play_random_sound' in res:
-                assert any('ага' or 'min_delay' in d for d in res)
-        if not any('nv.play_random_sound' in d for d in result):
-            assert False
-    # nv synth
-    with allure.step("nv_synth"):
-        count = 0
-        for res in result:
-            if 'nv.synthesize' in res:
-                count += 1
-                assert len(res[1]) > 20
-        assert count == 12
-        if not any('nv.synthesize' in d for d in result):
-            assert False
-    with allure.step("nv_listen и перебивание"):
-        # nv listen / перебивание
-        for res in result:
-            if 'nv.listen' in res:
-                for r in res:
-                    if "распознавание" in r:
-                        assert "распознавание" or "пока" in r
-                    elif "перебивание" in r:
-                        assert "перебивание" or "часть" or "смотреть" in r
-        if not any('nv.listen' in d for d in result):
-            assert False
-    with allure.step("nv_bridge"):
-        # ____ nv bridge
-        for res in result:
-            if 'nv.bridge' in res:
-                assert '555555' in res[1]
-        if not any('nv.bridge' in d for d in result):
-            assert False
+    for res in result:
+        if 'nv.say' in res:
+            assert any('hello' in d for d in res)
+    if not any('nv.say' in d for d in result):
+        assert False
 
-    synth_phrase_list = [res[1][res[1].find(':') + 4: res[1].find(',') - 1] for res in result if 'nv.synthesize' in res]
 
-    with allure.step("nn.env"):
-        assert 'Вытащили переменную окружения' in synth_phrase_list
+@allure.feature("Smoke 3.0")
+@allure.story("Проверка медиа части google")
+@allure.title('nv_background')
+def test_v3_media_part_google_background(app_3, db):
+    global result
+    for res in result:
+        if 'nv.background' in res:
+            assert any('Office_sound' in d for d in res)
+    if not any('nv.background' in d for d in result):
+        assert False
 
-    with allure.step('nn.storage'):
-        assert 'Хранилище работает' in synth_phrase_list
 
-    with allure.step('nn.counter'):
-        assert 'Счетчик работает' in synth_phrase_list
+@allure.feature("Smoke 3.0")
+@allure.story("Проверка медиа части google")
+@allure.title('nv_random_sound')
+def test_v3_media_part_google_play_random_sound(app_3, db):
+    global result
+    for res in result:
+        if 'nv.play_random_sound' in res:
+            assert any('ага' or 'min_delay' in d for d in res)
+    if not any('nv.play_random_sound' in d for d in result):
+        assert False
 
-    with allure.step('nn.has_record - valid'):
-        assert 'Проверка наличия записи работает' in synth_phrase_list
 
-    with allure.step('nn.has_record - no valid'):
-        assert 'Проверка наличия записи работает некорректно' not in synth_phrase_list
+@allure.feature("Smoke 3.0")
+@allure.story("Проверка медиа части google")
+@allure.title('nv_synthesize')
+def test_v3_media_part_google_synthesize(app_3, db):
+    global result
+    count = 0
+    for res in result:
+        if 'nv.synthesize' in res:
+            count += 1
+            assert len(res[1]) > 20
+    assert count == 12
+    if not any('nv.synthesize' in d for d in result):
+        assert False
 
-    with allure.step('nn.has_records - valid'):
-        assert 'Работает наличия записей проверка' in synth_phrase_list
 
-    with allure.step('nn.has_records - no valid'):
-        assert 'Проверка наличия записей работает некорректно' not in synth_phrase_list
+@allure.feature("Smoke 3.0")
+@allure.story("Проверка медиа части google")
+@allure.title('listen и перебивание')
+def test_v3_media_part_google_listen_interruption(app_3, db):
+    global result
+    for res in result:
+        if 'nv.listen' in res:
+            for r in res:
+                if "распознавание" in r:
+                    assert "распознавание" or "пока" in r
+                elif "перебивание" in r:
+                    assert "перебивание" in r
+    if not any('nv.listen' in d for d in result):
+        assert False
 
-    logs_dict = {}
-    for item in [res[1] for res in result if 'nn.log' in res]:
-        if 'city' in item:
-            logs_dict['extract_address'] = item
-        if 'first' in item:
-            logs_dict['extract_person'] = item
-        if 'bot' in item:
-            logs_dict['call_transcription'] = item
-        if len(item) <= 4:
-            logs_dict['call_duration'] = item
-        if 'no_input_timeout' in item:
-            logs_dict['get_default'] = item
 
-    with allure.step('nlu.extract_person'):
-        assert logs_dict['extract_person'] == "{'first': 'иван', 'last': 'петров', 'middle': 'алексеевич'}"
+@allure.feature("Smoke 3.0")
+@allure.story("Проверка медиа части google")
+@allure.title('nv_bridge')
+def test_v3_media_part_google_bridge(app_3, db):
+    global result
+    for res in result:
+        if 'nv.bridge' in res:
+            assert '555555' in res[1]
+    if not any('nv.bridge' in d for d in result):
+        assert False
 
-    # with allure.step('nlu.extract_address'):
-    #     assert logs_dict['extract_address'] == "{'city': ['москва', None], 'street': ['ленина', 'улица']," \
-    #                                            " 'building': ['16', None, None], 'appartment': '5'}"
 
-    with allure.step('nv.listen - тишина'):
-        listen_list = [res[1].split(',') for res in result if 'nv.listen' in res]
-        utterance_listen_list = [i[i.find(':') + 2:] for res in listen_list for i in res if
-                                 'utterance' in i]
-        assert 'null' in utterance_listen_list
-        for item in [res[1] for res in result if 'nn.dump' in res]:
-            assert 'error' not in item.lower()
+@allure.feature("Smoke 3.0")
+@allure.story("Проверка медиа части google")
+@allure.title('nn_env')
+def test_v3_media_part_google_nn_env(app_3, db):
+    global synth_phrase_list
+    assert 'Вытащили переменную окружения' in synth_phrase_list
 
-    with allure.step('get_call_transcription'):
-        for synth in synth_phrase_list:
-            assert synth in logs_dict['call_transcription']
 
-    with allure.step('get_call_duration'):
-        assert len(logs_dict['call_duration']) > 0
-        assert int(logs_dict['call_duration']) > 0
+@allure.feature("Smoke 3.0")
+@allure.story("Проверка медиа части google")
+@allure.title('nn_storage')
+def test_v3_media_part_google_nn_storage(app_3, db):
+    global synth_phrase_list
+    assert 'Хранилище работает' in synth_phrase_list
 
-    with allure.step('get_default'):
-        assert logs_dict['get_default'] == "{'no_input_timeout': 5000, 'recognition_timeout': 30000," \
-                                           " 'speech_complete_timeout': 5000, 'asr_complete_timeout': 5000}"
+
+@allure.feature("Smoke 3.0")
+@allure.story("Проверка медиа части google")
+@allure.title('nn_counter')
+def test_v3_media_part_google_nn_counter(app_3, db):
+    global synth_phrase_list
+    assert 'Счетчик работает' in synth_phrase_list
+
+
+@allure.feature("Smoke 3.0")
+@allure.story("Проверка медиа части google")
+@allure.title('nn_has_record valid')
+def test_v3_media_part_google_nn_has_record_valid(app_3, db):
+    global synth_phrase_list
+    assert 'Проверка наличия записи работает' in synth_phrase_list
+
+
+@allure.feature("Smoke 3.0")
+@allure.story("Проверка медиа части google")
+@allure.title('nn_has_record no valid')
+def test_v3_media_part_google_nn_has_record_no_valid(app_3, db):
+    global synth_phrase_list
+    assert 'Проверка наличия записи работает некорректно' not in synth_phrase_list
+
+
+@allure.feature("Smoke 3.0")
+@allure.story("Проверка медиа части google")
+@allure.title('nn_has_records_valid')
+def test_v3_media_part_google_nn_has_records_valid(app_3, db):
+    global synth_phrase_list
+    assert 'Работает наличия записей проверка' in synth_phrase_list
+
+
+@allure.feature("Smoke 3.0")
+@allure.story("Проверка медиа части google")
+@allure.title('nn_has_records no valid')
+def test_v3_media_part_google_nn_has_records_no_valid(app_3, db):
+    global synth_phrase_list
+    assert 'Проверка наличия записей работает некорректно' not in synth_phrase_list
+
+
+@allure.feature("Smoke 3.0")
+@allure.story("Проверка медиа части google")
+@allure.title('nlu_extract_person')
+def test_v3_media_part_google_nlu_extract_person(app_3, db):
+    global logs_dict
+    assert logs_dict['extract_person'] == "{'first': 'иван', 'last': 'петров', 'middle': 'алексеевич'}"
+
+
+@allure.feature("Smoke 3.0")
+@allure.story("Проверка медиа части google")
+@allure.title('nlu_extract_address')
+def test_v3_media_part_google_nlu_extract_address(app_3, db):
+    global logs_dict
+    assert logs_dict['extract_address'] == "{'city': ['москва', None], 'street': ['ленина', 'улица']," \
+                                           " 'building': ['16', None, None], 'appartment': '5'}"
+
+
+@allure.feature("Smoke 3.0")
+@allure.story("Проверка медиа части google")
+@allure.title('nv_listen тишина')
+def test_v3_media_part_google_nlu_extract_person(app_3, db):
+    global logs_dict
+    listen_list = [res[1].split(',') for res in result if 'nv.listen' in res]
+    utterance_listen_list = [i[i.find(':') + 2:] for res in listen_list for i in res if
+                             'utterance' in i]
+    assert 'null' in utterance_listen_list
+    for item in [res[1] for res in result if 'nn.dump' in res]:
+        assert 'error' not in item.lower()
+
+
+@allure.feature("Smoke 3.0")
+@allure.story("Проверка медиа части google")
+@allure.title('nv.get_call_transcription')
+def test_v3_media_part_google_nv_get_call_transcription(app_3, db):
+    global logs_dict
+    for synth in synth_phrase_list:
+        assert synth in logs_dict['call_transcription']
+
+
+@allure.feature("Smoke 3.0")
+@allure.story("Проверка медиа части google")
+@allure.title('nv.get_call_duration')
+def test_v3_media_part_google_nv_get_call_duration(app_3, db):
+    global logs_dict
+    assert len(logs_dict['call_duration']) > 0
+    assert int(logs_dict['call_duration']) > 0
+
+
+@allure.feature("Smoke 3.0")
+@allure.story("Проверка медиа части google")
+@allure.title('nv.get_default')
+def test_v3_media_part_google_nv_get_default(app_3, db):
+    global logs_dict
+    assert logs_dict['get_default'] == "{'no_input_timeout': 5000, 'recognition_timeout': 30000," \
+                                       " 'speech_complete_timeout': 5000, 'asr_complete_timeout': 5000}"
+
 
 #
 # @allure.feature("Silence")
