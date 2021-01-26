18160775cb5a29b4a7a0f8c9f76b77180f24e31e
Renat <rdevetiarov@neuro.net>
2021-01-26 09:25:13 +0300

    Убраны лишние методы - ТЕСТ

--- test_3/fixture/page.py
+++ test_3/fixture/page.py
@@ -8,39 +8,16 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
-class PageHelper:
+# class PageHelper:
+#
+#     def __init__(self, app_3):
+#         self.app = app_3
+#
+#     def go_to_project(self, uuid):
+#         wd = self.app.wd
+#         wd.get("https://cms-v3.neuro.net/agent-settings/settings?lng=en&agent_uuid=%s" % uuid)
+#
+#     def return_to_main(self):
+#         wd = self.app.wd
+#         wd.find_element_by_xpath("//div/span").click()
 
-    def __init__(self, app_3):
-        self.app = app_3
-
-    def go_to_project(self, uuid):
-        wd = self.app.wd
-        wd.get("https://cms-v3.neuro.net/agent-settings/settings?lng=en&agent_uuid=%s" % uuid)
-
-    def return_to_main(self):
-        wd = self.app.wd
-        wd.find_element_by_xpath("//div/span").click()
-
-    def check_menu(self):
-        wd = self.app.wd
-        # ---prod
-        # wd.find_element_by_xpath("//a[3]/div/span").click()
-        # wd.find_element_by_xpath("//a[4]/div/span").click()
-        # wd.find_element_by_xpath("//a[5]/div/span").click()
-        # wd.find_element_by_xpath("//a[6]/div").click()
-        # wd.find_element_by_xpath("//a[7]/div/span").click()
-        # wd.find_element_by_xpath("//a[8]/div/span").click()
-        # wd.find_element_by_xpath("//a[9]/div/span").click()
-        # wd.find_element_by_xpath("//a[10]/div").click()
-        # ---test
-        wd.find_element_by_xpath("//a[2]/div/span").click()
-        wd.find_element_by_xpath("//a[3]/div/span").click()
-        wd.find_element_by_xpath("//a[4]/div/span").click()
-        wd.find_element_by_xpath("//a[5]/div").click()
-        wd.find_element_by_xpath("//a[6]/div/span").click()
-        wd.find_element_by_xpath("//a[7]/div").click()
-        wd.find_element_by_xpath("//a[8]/div").click()
-        wd.find_element_by_xpath("//a[9]/div").click()
-        wd.find_element_by_xpath("//a[10]/div/span").click()
-        wd.find_element_by_xpath("//a[11]/div").click()
-        # wd.find_element_by_xpath("//div/span").click()
