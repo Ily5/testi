df2290ad94c243016bc86518361768722da96f08
Renat <rdevetiarov@neuro.net>
2021-02-04 17:04:06 +0300

    [Load LE offline] Доаботка скрипта загрузки диалогов, начало метода чека логов лоджика

--- test_3/test_load/test_load_le_offline.py
+++ test_3/test_load/test_load_le_offline.py
@@ -1,4 +1,5 @@
 import time
+import paramiko
 
 
 def test_load_logic_offline(api_v3, pool_api_v3, creation_queue_dialog, params_agent_uuid):
@@ -10,48 +11,50 @@ def test_load_logic_offline(api_v3, pool_api_v3, creation_queue_dialog, params_a
     print('\n--(Количество диалогов до)-- ', response_before.json()['total'])
 
     path_agent_setting = api_v3.path_end_point['put_change_agent_settings']
-    api_v3.request_send(method="PUT", path=path_agent_setting, json={'total_channel_limit': 1000000},
-                        params=params_agent_uuid)
-    time_out = time.time() + 60
+    res = api_v3.request_send(method="PUT", path=path_agent_setting, json={'total_channel_limit': 100000},
+                              params=params_agent_uuid)
+    print('Поменяли TCL на ', res.json()['total_channel_limit'])
 
-    calls_uuid_set = set()
+    # total = api_v3.request_send(path=api_v3.path_end_point['get_agent_settings'], params=params_agent_uuid).json()
+    # print(total['total_channel_limit'])
+
+    set_dialogs_status = set()
     while True:
-        response_calls = pool_api_v3.request_send(path=pool_api_v3.path_end_point['get_list_queue_calls'],
-                                                  params=params).json()['calls']
-        for call in response_calls:
-            if call['uuid'] not in calls_uuid_set:
-                calls_uuid_set.add(call['uuid'])
+        resp = pool_api_v3.request_send(path=path, params=params)
+        for dialog in resp.json()['dialogs']:
+            set_dialogs_status.add(dialog['result'])
+        if 'queued' in set_dialogs_status:
+            break
+
+    count = 0
+    time_out = time.time() + 60
+    print('Отчет времени старт')
+    while True:
+        count += 1
+        print(count, '  --- ', time.time(), '  --- ', time_out)
+        time.sleep(1)
 
         if time.time() >= time_out:
-            response_after = pool_api_v3.request_send(path=path, params=params)
-            print('\n--(After)--', response_after.json())
-
-            print('\n--(Количество диалогов после)-- ', response_after.json()['total'])
-
-            response_1 = pool_api_v3.request_send(path=pool_api_v3.path_end_point['get_list_queue_calls'],
-                                                  params=params)
-            print('\n --(calls)--', response_1.json())
-            for call in response_1.json()['calls']:
-                if call['uuid'] not in calls_uuid_set:
-                    calls_uuid_set.add(call['uuid'])
-
+            print('Отчет времени стоп')
             break
-    print('\n--(Len calls)--', len(calls_uuid_set))
 
 
-def test_01(params_agent_uuid, pool_api_v3, api_v3):
-    while True:
-        time.sleep(50)
-        path = pool_api_v3.path_end_point['get_all_dialog_queue']
-        params = {**{"page": "1",
-                     "by_count": "100"}, **params_agent_uuid}
-        response = pool_api_v3.request_send(path=path, params=params)
-        print('\n--(2)--', response.json()['total'])
-        # response_1 = pool_api_v3.request_send(path=pool_api_v3.path_end_point['get_list_queue_calls'],
-        #                                       params=params)
-        # print('\n --(calls)--', response_1.json())
-        if response.json()['total'] > 5000:
-            api_v3.request_send(method='POST', path=api_v3.path_end_point['remove_queue_dialogs'],
-                                params=params_agent_uuid,
-                                status_code=409)
+def check_log():
+    host = '10.131.0.60'
+    user = 'rdevetiarov'
+    my_key = '/home/renat/.ssh/id_rsa.pub'
 
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname=host, username=user)
+    result = client.exec_command("cat /var/log/ivr/logic-executor-offline.log | grep 'creating outbound call' | wc -l")
+    before = result[1].read()
+    print(before)
+
+    result = client.exec_command('tail -n 10  /var/log/ivr/logic-executor-offline.log')
+    data = result[1].read() + result[2].read()
+    print(data)
+    client.close()
+
+
+check_log()
