cb0f5b4d74240854f11bcb8f1f12edcdfb0c9d9e
Renat <rdevetiarov@neuro.net>
2021-02-17 13:44:27 +0300

    Tmp fix

--- conftest.py
+++ conftest.py
@@ -140,7 +140,7 @@ def mdb(request):
 
 @pytest.fixture()
 def file_helper():
-    helper = FileHelper(path_to_file='/home/')
+    helper = FileHelper(path_to_file='/var/jenkins_home')
     return helper
 
 
