b8840a809f374e45fb3484628ec20c0fc2b757f7
Renat <rdevetiarov@neuro.net>
2021-02-28 13:26:09 +0300

    change default stand

--- conftest.py
+++ conftest.py
@@ -162,7 +162,7 @@ def random_str_generator(size=random.randint(3, 129),
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="remote")
-    parser.addoption("--config", action="store", default=ROOT_DIR + "/config_test.json")
+    parser.addoption("--config", action="store", default=ROOT_DIR + "/config_prod.json")
 
 # TODO : make universal parametrize method in future
 # def pytest_make_parametrize_id(val):
