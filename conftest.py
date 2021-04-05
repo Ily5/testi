1124849c1403d08ffec177793f2beab7aaf3991c
Renat <rdevetiarov@neuro.net>
2021-04-05 10:59:01 +0300

    [conftest.py] -  Mini refactor

--- conftest.py
+++ conftest.py
@@ -1,21 +1,25 @@
 import string
 import random
 import os
-import json
 import time
+import json
+
 import pytest
 
 from fixture.application import Application
 from fixture.database import Connector, MongoConnector
-from test_3.fixture.api import APIClientV3
-from test_3.fixture.application_3 import ApplicationNewVersion
 from test_3.fixture.Helper import FileHelper, SshHelper
 
-# fixture = None
+from test_3.fixture.application_3 import ApplicationNewVersion
+
+from test_3.fixture.ApiWF.ExternalApi import ExternalApi
+from test_3.fixture.ApiWF.CmsApi import CmsApi
+from test_3.fixture.ApiWF.PoolApi import PoolApi
+from test_3.fixture.ApiWF.NluApi import NluApi
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-''' Фикстуры для V2'''
+""" Фикстуры для V2"""
 
 
 @pytest.fixture(scope="session")
@@ -26,14 +30,24 @@ def app(request):
         browser = request.config.getoption("--browser")
         with open(request.config.getoption("--config")) as cfg:
             config = json.load(cfg)
-            fixture = Application(browser=browser, cms_url=config["CmsUrl"], api_url=config["ApiEndpoint"],
-                                  api_headers=config["ApiHeaders"], api_methods=config["ApiMethods"],
-                                  pool_api=config["PoolApiUrl"], p_api_headers=config["PoolApiHeaders"],
-                                  project=config["ProjectId"], rwdb=config["Postgres"]["RW"],
-                                  cms_db=config["Postgres"]["CMS"], mdb=config["Mongo_client"],
-                                  speech_engine=config["speech_engine"],
-                                  database=config["database"])
-            fixture.session.login(username=config["UsernameCms"], password=config["PasswordCms"])
+            fixture = Application(
+                browser=browser,
+                cms_url=config["CmsUrl"],
+                api_url=config["ApiEndpoint"],
+                api_headers=config["ApiHeaders"],
+                api_methods=config["ApiMethods"],
+                pool_api=config["PoolApiUrl"],
+                p_api_headers=config["PoolApiHeaders"],
+                project=config["ProjectId"],
+                rwdb=config["Postgres"]["RW"],
+                cms_db=config["Postgres"]["CMS"],
+                mdb=config["Mongo_client"],
+                speech_engine=config["speech_engine"],
+                database=config["database"],
+            )
+            fixture.session.login(
+                username=config["UsernameCms"], password=config["PasswordCms"]
+            )
 
     def done():
         try:
@@ -46,67 +60,99 @@ def app(request):
     return fixture
 
 
-''' Фикстуры для V3'''
+""" Фикстуры для V3"""
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def api_v3(request):
     fixture = None
     if fixture is None:
         with open(request.config.getoption("--config")) as cfg:
             config = json.load(cfg)
-            with open(ROOT_DIR + "/config_v3.json", 'r', encoding='UTF-8') as conf:
+            with open(ROOT_DIR + "/config_v3.json", "r", encoding="UTF-8") as conf:
                 config_v3 = json.load(conf)
-                fixture = APIClientV3(base_url=config['api']['external_api_base_url'],
-                                      test_data=config['test_data'],
-                                      path_end_point=config_v3['api']['external_api'],
-                                      database=config["Postgres"])
-                token, refresh_token = fixture.get_api_token(password=config['auth']['pass'],
-                                                             login=config['auth']['login'])
+                fixture = ExternalApi(
+                    base_url=config["api"]["external_api_base_url"],
+                    test_data=config["test_data"],
+                    path_end_point=config_v3["api"]["external_api"],
+                    database=config["Postgres"],
+                )
+                token, refresh_token = fixture.get_api_token(
+                    password=config["auth"]["pass"], login=config["auth"]["login"]
+                )
             fixture.token, fixture.refresh_token = token, refresh_token
 
     return fixture
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
+def cms_api_v3(request):
+    fixture = None
+    if fixture is None:
+        with open(request.config.getoption("--config")) as cfg:
+            config = json.load(cfg)
+            with open(ROOT_DIR + "/config_v3.json", "r", encoding="UTF-8") as conf:
+                config_v3 = json.load(conf)
+                fixture = CmsApi(
+                    base_url=config["api"]["external_api_base_url"],
+                    test_data=config["test_data"],
+                    path_end_point=config_v3["api"]["cms_api"],
+                    database=config["Postgres"],
+                )
+                token, refresh_token = fixture.get_api_token(
+                    password=config["auth"]["pass"], login=config["auth"]["login"]
+                )
+            fixture.token, fixture.refresh_token = token, refresh_token
+
+    return fixture
+
+
+@pytest.fixture(scope="session")
 def nlu_api_v3(request):
     fixture = None
     if fixture is None:
         with open(request.config.getoption("--config")) as cfg:
             config = json.load(cfg)
-            with open(ROOT_DIR + "/config_v3.json", 'r', encoding='UTF-8') as conf:
+            with open(ROOT_DIR + "/config_v3.json", "r", encoding="UTF-8") as conf:
                 config_v3 = json.load(conf)
-                fixture = APIClientV3(base_url=config['api']['nlu_api_url'],
-                                      path_end_point=config_v3['api']['nlu_api'])
+                fixture = NluApi(
+                    base_url=config["api"]["nlu_api_url"],
+                    path_end_point=config_v3["api"]["nlu_api"],
+                )
     return fixture
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def pool_api_v3(request):
     fixture = None
     if fixture is None:
         with open(request.config.getoption("--config")) as cfg:
             config = json.load(cfg)
-            with open(ROOT_DIR + "/config_v3.json", 'r', encoding='UTF-8') as conf:
+            with open(ROOT_DIR + "/config_v3.json", "r", encoding="UTF-8") as conf:
                 config_v3 = json.load(conf)
-                fixture = APIClientV3(base_url=config['api']['poll_api_v3_url'],
-                                      test_data=config['test_data'],
-                                      path_end_point=config_v3['api']["poll_api"])
+                fixture = PoolApi(
+                    base_url=config["api"]["poll_api_v3_url"],
+                    test_data=config["test_data"],
+                    path_end_point=config_v3["api"]["poll_api"],
+                )
     return fixture
 
 
-@pytest.fixture(scope='class')
+@pytest.fixture(scope="class")
 def app_3_web(request):
     fixture = None
     if fixture is None:
         browser = request.config.getoption("--browser")
         with open(request.config.getoption("--config")) as cfg:
             config = json.load(cfg)
-        fixture = ApplicationNewVersion(browser=browser, cms_url=config["CmsUrl3"], test_data=config,
-                                        database=None)
+        fixture = ApplicationNewVersion(
+            browser=browser, cms_url=config["CmsUrl3"], test_data=config, database=None
+        )
         fixture.wd.get(fixture.cms_url)
-        fixture.LoginPage.login_in_cms(username=fixture.test_data['auth']['login'],
-                                       password=fixture.test_data['auth']['pass'])
+        fixture.LoginPage.login_in_cms(
+            username=fixture.test_data["auth"]["login"],
+            password=fixture.test_data["auth"]["pass"],
+        )
         time.sleep(3)
 
     def done():
@@ -116,7 +162,7 @@ def app_3_web(request):
     return fixture
 
 
-'''Общие фикстуры '''
+"""Общие фикстуры """
 
 
 @pytest.fixture(scope="session")
@@ -140,7 +186,7 @@ def mdb(request):
 
 @pytest.fixture()
 def file_helper(api_v3):
-    helper = FileHelper(path_to_file='/var/jenkins_home/call_files/', api_helper=api_v3)
+    helper = FileHelper(path_to_file="/var/jenkins_home/call_files/", api_helper=api_v3)
     # helper = FileHelper(path_to_file='/home/renat/', api_helper=api_v3)
     return helper
 
@@ -149,15 +195,20 @@ def file_helper(api_v3):
 def ssh_helper(request):
     with open(request.config.getoption("--config")) as cfg:
         config = json.load(cfg)
-        fixture = SshHelper(username=config['test_data']['ssh']['login'], hosts=config['test_data']['ssh']['hosts'])
+        fixture = SshHelper(
+            username=config["test_data"]["ssh"]["login"],
+            hosts=config["test_data"]["ssh"]["hosts"],
+        )
 
     return fixture
 
 
 @pytest.fixture
-def random_str_generator(size=random.randint(3, 129),
-                         chars=string.ascii_uppercase + string.digits + string.ascii_lowercase + '\t'):
-    return ''.join(random.choice(chars) for _ in range(size))
+def random_str_generator(
+        size=random.randint(3, 129),
+        chars=string.ascii_uppercase + string.digits + string.ascii_lowercase + "\t",
+):
+    return "".join(random.choice(chars) for _ in range(size))
 
 
 @pytest.fixture()
@@ -167,8 +218,13 @@ def get_asr_engine(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="remote")
-    parser.addoption("--config", action="store", default=ROOT_DIR + "/config_prod.json")
-    parser.addoption("--asr", action="store", default="yandex", help="Set ars engine: yandex or google")
+    parser.addoption("--config", action="store", default=ROOT_DIR + "/config_test.json")
+    parser.addoption(
+        "--asr",
+        action="store",
+        default="yandex",
+        help="Set ASR engine: yandex or google",
+    )
 
 # TODO : make universal parametrize method in future
 # def pytest_make_parametrize_id(val):
