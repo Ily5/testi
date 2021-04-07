a7a052feb26f9de9804badc5c8d7a618c2692244
Renat <rdevetiarov@neuro.net>
2021-04-07 16:57:36 +0300

    [CmsApi] Bag fix, add description

--- test_3/fixture/ApiWF/CmsApi.py
+++ test_3/fixture/ApiWF/CmsApi.py
@@ -18,19 +18,30 @@ class CmsApi(APIClientV3):
                 nn_logs[action["name"]] = action["data"]
         return nn_logs
 
-
     def get_calls_agent_in_queue(
-        self,
-        agent_uuid: str,
-        msisdn: Optional[str] = None,
-        result: Optional[str] = None,
-        bulk_uuid: Optional[str] = None,
-        call_uuid: Optional[str] = None,
-        sort: Optional[str] = None,
-        field_sort: Optional[str] = None,
-        limit=5000,
-        offset=0,
+            self,
+            agent_uuid: str,
+            msisdn: Optional[str] = None,
+            result: Optional[str] = None,
+            bulk_uuid: Optional[str] = None,
+            call_uuid: Optional[str] = None,
+            sort: Optional[str] = None,
+            field_sort: Optional[str] = None,
+            limit=2000,
+            offset=0,
     ) -> requests.Response:
+        """" Метод получения звонков агента
+        :param agent_uuid: обязательный параметр, передаем agent_uuid
+        :param msisdn: необязательно, передаем для фильтрации по номеру
+        :param result: необязательо, передаем для фильтрации по статусу
+        :param bulk_uuid: необязательно, передаем для фильтрации по bulk_uuid
+        :param call_uuid: необязательно, передаем для для фильтрации по call_uuid
+        :param sort: необязательно, передаем для активации сортировки: asc, desc, так же небходимо передать field_sort
+        :param field_sort: обязательно, если sort is not None, передаем выбора параметра сортировки: result, date_added
+        :param limit: количество выводимых звонков, по дефолту 2000
+        :param offset: количетсов пропускаемых звонков, по дефолту 0
+        """
+
         request_data = {
             "limit": limit,
             "offset": offset,
@@ -39,11 +50,11 @@ class CmsApi(APIClientV3):
         if msisdn is not None:
             request_data['where']["msisdn"].append(msisdn)
         if result is not None:
-            request_data['where']["msisdn"].append(result)
+            request_data['where']["result"].append(result)
         if call_uuid is not None:
             request_data["where"]["call_uuid"] = call_uuid
         if bulk_uuid is not None:
-            request_data["where"]["call_uuid"] = bulk_uuid
+            request_data["where"]["bulk_uuid"] = bulk_uuid
         if sort is not None:
             if sort not in ["asc", "desc"]:
                 raise ValueError("Param sort must be asc or desc")
@@ -52,10 +63,8 @@ class CmsApi(APIClientV3):
             request_data["sort"] = {}
             request_data["sort"][field_sort] = sort
 
-        resp = self.request_send(
+        return self.request_send(
             method="POST",
             path=self.path_end_point["get_calls_agent_in_queue"],
             json=request_data,
         )
-        print(resp.request.body)
-        return resp
